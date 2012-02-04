6663d3b0bf4d65ce59e1c355e96522175b89cf3d
Sang Tae Park <pastime1971@gmail.com>
2012-02-04 05:51:00 -0500

    AbsListView: add option to disable scrolling cache (2/2) Ported from gingerbread
    scrolling cache can cause performance lag due to GC
    allow user to enable/disable/force scrolling cache on the fly
    via Settings > Performance > Scrolling cache
    
    Change-Id: I6826a7e2036f2fc4f6044e2c197c38c278bcdf21

--- src/com/android/settings/cyanogenmod/PerformanceSettings.java
+++ src/com/android/settings/cyanogenmod/PerformanceSettings.java
@@ -59,6 +59,14 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
     private static final String FORCE_HIGHEND_GFX_PREF = "pref_force_highend_gfx";
     private static final String FORCE_HIGHEND_GFX_PERSIST_PROP = "persist.sys.force_highendgfx";
 
+    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+
+    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+
+    private static final String SCROLLINGCACHE_DEFAULT = "2";
+
+    private ListPreference mScrollingCachePref;
+
     public static final String KEY_FORCE_FAST_CHARGE = "pref_force_fast_charge";
     public static final String FAST_CHARGE_PATH = fastchargePath();
 
@@ -119,6 +127,11 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
             setCurrentValue();
             mPerfProfilePref.setOnPreferenceChangeListener(this);
         }
+
+        mScrollingCachePref = (ListPreference) prefSet.findPreference(SCROLLINGCACHE_PREF);
+        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+        mScrollingCachePref.setOnPreferenceChangeListener(this);
 
         category = (PreferenceCategory) prefSet.findPreference(CATEGORY_GRAPHICS);
         mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
@@ -200,6 +213,9 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
             if (preference == mPerfProfilePref) {
                 mPowerManager.setPowerProfile(String.valueOf(newValue));
                 setCurrentPerfProfileSummary();
+                return true;
+            } else if (preference == mScrollingCachePref) {
+                SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String)newValue);
                 return true;
             }
         }
