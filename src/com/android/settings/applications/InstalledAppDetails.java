cc7282d8e904eb50958b4103527f4f26d464f8a0
ByteHamster <info@bytehamster.com>
2014-03-05 15:08:17 +0100

    Settings: Add AppOps link to app details
    http://jira.omnirom.org/browse/OMNI-69
    
    Change-Id: Id9269dac81a3dc5e27072bd727244b9417bcbba8

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -142,6 +142,7 @@ public class InstalledAppDetails extends Fragment
     private Button mForceStopButton;
     private Button mClearDataButton;
     private Button mMoveAppButton;
+    private Button mAppOpsButton;
     private CompoundButton mNotificationSwitch;
 
     private PackageMoveObserver mPackageMoveObserver;
@@ -526,6 +527,7 @@ public class InstalledAppDetails extends Fragment
         mEnableCompatibilityCB = (CheckBox) view.findViewById(R.id.enable_compatibility_cb);
         
         mNotificationSwitch = (CompoundButton) view.findViewById(R.id.notification_switch);
+        mAppOpsButton = (Button) view.findViewById(R.id.app_ops_button);
 
         return view;
     }
@@ -654,6 +656,21 @@ public class InstalledAppDetails extends Fragment
         // Set application name.
         TextView label = (TextView) appSnippet.findViewById(R.id.app_name);
         label.setText(mAppEntry.label);
+        // Go to AppOps button
+        mAppOpsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle args = new Bundle();
+                args.putString(AppOpsDetails.ARG_PACKAGE_NAME, mAppEntry.info.packageName);
+
+                AppOpsDetails appOps = new AppOpsDetails();
+                appOps.setArguments(args);
+                getFragmentManager().beginTransaction()
+                .add(android.R.id.content, appOps)
+                .addToBackStack(null)
+                .commit();
+            }
+        });
         // Version number of application
         mAppVersion = (TextView) appSnippet.findViewById(R.id.app_size);
 
