PMStaging/android_packages_apps_Settings

[2/2] Settings: Lockscreen notifications: Option to disable dynamic width
Change-Id: I79b76134e9ff29ea5e305129870d6c5517907203

src/com/android/settings/pac/lsn/LockscreenNotifications.java
@@ -36,6 +36,7 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
     private static final String KEY_WAKE_ON_NOTIFICATION = "wake_on_notification";
     private static final String KEY_NOTIFICATIONS_HEIGHT = "notifications_height";
     private static final String KEY_PRIVACY_MODE = "privacy_mode";
+    private static final String KEY_DYNAMIC_WIDTH = "dynamic_width";
     private static final String KEY_OFFSET_TOP = "offset_top";
     private static final String KEY_CATEGORY_GENERAL = "category_general";
     private static final String KEY_EXCLUDED_APPS = "excluded_apps";
@@ -52,6 +53,7 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
     private CheckBoxPreference mForceExpandedView;
     private NumberPickerPreference mNotificationsHeight;
     private CheckBoxPreference mPrivacyMode;
+    private CheckBoxPreference mDynamicWidth;
     private SeekBarPreference mOffsetTop;
     private AppMultiSelectListPreference mExcludedAppsPref;
     private ColorPickerPreference mNotificationColor;
@@ -102,6 +104,11 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
         mPrivacyMode.setChecked(Settings.PAC.getInt(cr,
                     Settings.PAC.LOCKSCREEN_NOTIFICATIONS_PRIVACY_MODE, 0) == 1);
         mPrivacyMode.setEnabled(mLockscreenNotifications.isChecked());
+
+        mDynamicWidth = (CheckBoxPreference) prefs.findPreference(KEY_DYNAMIC_WIDTH);
+        mDynamicWidth.setChecked(Settings.PAC.getInt(cr,
+                    Settings.PAC.LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH, 1) == 1);
+        mDynamicWidth.setEnabled(mLockscreenNotifications.isChecked());
 
         mExpandedView = (CheckBoxPreference) prefs.findPreference(KEY_EXPANDED_VIEW);
         mExpandedView.setChecked(Settings.PAC.getInt(cr,
@@ -206,6 +213,9 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
             mForceExpandedView.setEnabled(mLockscreenNotifications.isChecked() && mExpandedView.isChecked()
                         && !mPrivacyMode.isChecked());
             mExpandedView.setEnabled(mLockscreenNotifications.isChecked() && !mPrivacyMode.isChecked());
+        } else if (preference == mDynamicWidth) {
+            Settings.PAC.putInt(cr, Settings.PAC.LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH,
+                    mDynamicWidth.isChecked() ? 1 : 0);
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }

SpiritBlood/packages_apps_Settings

src/com/android/settings/crdroid/LockscreenNotifications.java
@@ -46,6 +46,7 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
     private static final String KEY_WAKE_ON_NOTIFICATION = "wake_on_notification";
     private static final String KEY_NOTIFICATIONS_HEIGHT = "notifications_height";
     private static final String KEY_PRIVACY_MODE = "privacy_mode";
+    private static final String KEY_DYNAMIC_WIDTH = "dynamic_width";
     private static final String KEY_OFFSET_TOP = "offset_top";
     private static final String KEY_CATEGORY_GENERAL = "general_category";
     private static final String KEY_EXCLUDED_APPS = "excluded_apps";
@@ -67,6 +68,7 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
     private CheckBoxPreference mForceExpandedView;
     private NumberPickerPreference mNotificationsHeight;
     private CheckBoxPreference mPrivacyMode;
+    private CheckBoxPreference mDynamicWidth;
     private SeekBarPreference mOffsetTop;
     private AppMultiSelectListPreference mExcludedAppsPref;
     private ColorPickerPreference mNotificationColor;
@@ -151,6 +153,11 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
         mPrivacyMode.setChecked(Settings.System.getInt(cr,
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_PRIVACY_MODE, 0) == 1);
         mPrivacyMode.setEnabled(mLockscreenNotifications.isChecked());
+
+        mDynamicWidth = (CheckBoxPreference) prefs.findPreference(KEY_DYNAMIC_WIDTH);
+        mDynamicWidth.setChecked(Settings.System.getInt(cr,
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH, 1) == 1);
+        mDynamicWidth.setEnabled(mLockscreenNotifications.isChecked());
 
         mExpandedView = (CheckBoxPreference) prefs.findPreference(KEY_EXPANDED_VIEW);
         mExpandedView.setChecked(Settings.System.getInt(cr,
@@ -296,6 +303,9 @@ public class LockscreenNotifications extends SettingsPreferenceFragment implemen
             mForceExpandedView.setEnabled(mLockscreenNotifications.isChecked() && mExpandedView.isChecked()
                         && !mPrivacyMode.isChecked());
             mExpandedView.setEnabled(mLockscreenNotifications.isChecked() && !mPrivacyMode.isChecked());
+        } else if (preference == mDynamicWidth) {
+            Settings.System.putInt(cr, Settings.System.LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH,
+                    mDynamicWidth.isChecked() ? 1 : 0);
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }